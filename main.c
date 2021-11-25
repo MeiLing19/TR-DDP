ef5e8ec89ea1febae9d2c573510da0a2f747943f
yosafatrend <yosafatrendip@gmail.com>
2021-11-25 12:40:38 +0700

    menambahkan fitur menu

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void rumahPola();
+void tokoWarisanFTI();
+void bendera();
+void author();
+void exitt();
+
 void main(){
     int menu;
-    char ans;
 
-
+    system("cls");
     printf("TR DDP : \n");
     printf("1. Rumah Pola \n");
     printf("2. Toko Warisan FTI \n");
@@ -13,11 +18,55 @@ void main(){
     printf("4. Author \n");
     printf("5. Exit \n");
     printf("Pilihan : ");
-    //tes commit kedua
-    scanf("%d", &menu);
-    /*switch(menu)
-    {
-        case 1: system("cls")
-    }*/
 
+    scanf("%d", &menu);
+    switch(menu)
+    {
+        system("cls");
+        case 1:
+            system("cls");
+            rumahPola();
+            break;
+        case 2:
+            system("cls");
+            tokoWarisanFTI();
+            break;
+        case 3:
+            system("cls");
+            bendera();
+            break;
+        case 4:
+            system("cls");
+            author();
+            break;
+        case 5:
+            system("cls");
+            exitt();
+            break;
+        default:
+            printf("\nPilihan salah, enter untuk mengulang");
+            getch();
+            main();
+            break;
+    }
+}
+
+void rumahPola(){
+    printf("haloo ini rumah pola");
+}
+
+void tokoWarisanFTI(){
+    printf("haloo ini rumah pola");
+}
+
+void bendera(){
+    printf("haloo ini rumah pola");
+}
+
+void author(){
+    printf("haloo ini rumah pola");
+}
+
+void exitt(){
+    printf("haloo ini rumah pola");
 }
